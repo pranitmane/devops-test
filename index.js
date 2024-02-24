d97c54a7b132e8bfaf2a5b3d9c396d50cf72ad90
rational09 <malharchikhale0@gmail.com>
2024-02-24 09:15:47 +0000

    from ec2

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/newFeature', (req, res) => {
     }
 );
 
+app.get('/newFeature-from-ec2', (req, res) => {
+    res.send('Hello World from new feature');
+    }
+);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
